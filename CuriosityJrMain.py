70e369c49824288f71cd433b261867b7f59d9fd6
Carlos Flores <cflor083@fiu.edu>
2016-04-08 14:25:00 -0400

    Started on the meat of the maincode, created motor objects and arm objects, as well as impoted any libraries that we may need.

--- CuriosityJrMain.py
+++ CuriosityJrMain.py
@@ -1,12 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import os
+import numpy as np
+import cv2
+import argparse
+import imutils
+from collections import deque
 import sys
 sys.path.append("Arms/")
-from testfor import * #* imports all classes from the module
-sys.path.append("Tracks/")
-from Test import *
+import mcp3008
+import irdist
+from arms_module import *
+sys.path.append("CamTracking/")
+import Tracking
+import cvDistance
 
-Sumthing = Sum(1,2)
+GPIO.setmode(GPIO.BCM)#Sets the pin Numbering system to GPIO scheme
+GPIO.setwarnings(False)
 
-Sumthing.PrintandSum()
+U = 1 #Enums for Controlling the Motors
+D = 0
+R = U
+L = D
 
-other = Sum2(3,4)
-other.PrintandSum()
+#Motor(UpPin,DownPin). Below initializes motor objects
+m1 = Motor(21,20)
+m2 = Motor(13,6)
+m3 = Motor(16,12)
+m4 = Motor(26,19)
+#DOWN is close, UP is open
+#m1-m4 are motors for the arm
+
+#Ltrackm = Motor(x,w)     Set the pins for the Left Track later
+#Rtrackm = Motor(y,z)     Same for the Right Track
+
+arm1 = Arm(m1,m2,m3,m4)
+
