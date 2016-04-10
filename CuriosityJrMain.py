7f339bdb8303a8dd1dc045ae4d5e1776b05fca53
mgarciacruzz <mgarciacruzz@gmail.com>
2016-04-09 23:15:52 -0400

    test6

--- CuriosityJrMain.py
+++ CuriosityJrMain.py
@@ -56,8 +56,8 @@ xcenter = 0 #x-coordinate of the center of the camera
 jr = Rover(arm1,tracks)
 
 print("before Rover")
-jr.navigate(8,1)
-jr.lunge(8)
+jr.navigate(9,1)
+jr.lunge(6)
 #below is experimental code that will execute.
 '''
 while(start == 0):
